079b635f5bd11db56593fe8ca3127d560c6cfd25
PenguinBKDN <son72ltv2@gmail.com>
2023-03-15 12:39:02 +0700

    txt

---
+++ src/public_html/main.js
@@ -0,0 +1,31 @@
+// function showModal() {
+//     loginModal.classList.add('open');
+//   }
+
+//   function hideModal() {
+//     loginModal.classList.remove('open');
+//   }
+
+// (() => {
+//     console.log("Hello");
+//     const login = document.querySelector('.js-click-login')
+//     const loginModal = document.querySelector('.js-user_modal')
+//     const modalContainer = document.querySelector('.js-login_container')
+//     const modalClose = document.querySelector('.js-modal-close')  
+    
+//     if (login !== null)
+//     {
+//         login.addEventListener('click', showModal);
+//     }
+//     else{
+//         console.log("Hello");
+//     }
+
+//     modalClose.addEventListener('click', hideModal);
+
+//     loginModal.addEventListener('click', hideModal);
+
+//     modalContainer.addEventListener('click', function (event) {
+//       event.stopPropagation()
+// })
+// })()
