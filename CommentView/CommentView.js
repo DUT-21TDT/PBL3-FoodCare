3a3fe9964d5f9ef83189466b533cb5d497cfaa4a
unknown <nguyenthuchoang17112003@gmail.com>
2023-04-10 21:59:50 +0700

    UpdateCommentView

---
+++ CommentView/CommentView.js
@@ -0,0 +1,36 @@
+// var urlImg = "../EditProfile/assets/img/coffee_noel_final_3_2.jpg";
+// var Name = "Nguyen Thuc Hoang";
+// var text = "Tat Den Di Tra Lai Tam Tri Toi Day, Meo Meo Meo Meo";
+// var commentMessageElement = document.getElementById("commentMessage");
+// var divCommentElement = document.getElementById("DivComment");
+
+
+// commentMessageElement.onclick = function(event)
+// {
+//     event.preventDefault();
+//     createComment(urlImg,Name,text);
+// }
+// function createComment(urlImg,name,text)
+// {
+//     let newComment = document.createElement("div");
+//     newComment.innerHTML(`
+//     <a href="" class="Comment__avatar">
+//     <div class="avatar-img">
+//         <img src="${urlImg}" alt="">
+//     </div>
+// </a>
+// <div class = "CommentByUser">
+//     <h4 class = "CommentByUser__name">${name}</h4>
+//     <span class = "CommentByUser__text">${text}
+//     </span>
+// </div>
+// </div>
+//     `);
+//     divCommentElement.appendChild(newComment);
+
+// }
+
+
+
+
+
