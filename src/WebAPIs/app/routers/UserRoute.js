42065bf50d587a17cf554ee91cc51189bf1abaad
Lighttx26 <ncnhatquang26@gmail.com>
2023-04-23 19:13:14 +0700

    Comment something

--- src/WebAPIs/app/routers/UserRoute.js
+++ src/WebAPIs/app/routers/UserRoute.js
@@ -3,8 +3,6 @@ const userRouter = express.Router();
 const userController = require("../controllers/UserController.js");
 const foodController = require("../controllers/FoodController.js");
 
-userRouter.get("/foods", foodController.getAllFoods);
-userRouter.get("/foods/:id", foodController.getFoodByID);
 
 userRouter.put("/:id/change_password", userController.changePassword);
 
