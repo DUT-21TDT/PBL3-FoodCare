4d813af04985b236cefced5628028f219a0986d6
Lighttx26 <ncnhatquang26@gmail.com>
2023-05-25 15:30:27 +0700

    Public route, add view public menu function

--- src/WebAPIs/app/routers/Guest/index.js
+++ src/WebAPIs/app/routers/Guest/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mainRouter = express.Router();
 
-mainRouter.use("/foods", require("../Guest/food.js"));
-mainRouter.use("/menus", require("../Guest/menu.js"));
+mainRouter.use("/public/foods", require("../Guest/food.js"));
+mainRouter.use("/public/menus", require("../Guest/menu.js"));
 
 module.exports = mainRouter;
